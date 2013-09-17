73ab5b8be62dbbe675675099f54f2140b2d7a692
Joseph Simoes <Joe@new-host-3.home>
2013-09-16 21:40:00 -0400

    Giving accurate change for most numbers

--- change_machine.rb
+++ change_machine.rb
@@ -9,7 +9,23 @@ class Register
   nickels = change/5
   pennies = change/1
 
+  remainder = change%25
+  d = remainder/10
+  n =  remainder/5
+  p = remainder/1
+  
+ 
 
-print "You get " + quarters.to_s + " quarters \n"
-
+ 
+  print quarters.to_s + " quarters \n"  
+  if remainder >= 10
+	print d.to_s + " dimes \n"
+  elsif remainder >= 5 
+	print n.to_s + " nickels\n"
+  elsif remainder >= 1
+	print p.to_s + " pennies\n"
+ 
+  end
+  
 end
+
