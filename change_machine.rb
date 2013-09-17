52f828a6adc95bbbe363f290cbf35b658c324425
Joseph Simoes <Joe@new-host-3.home>
2013-09-17 19:07:25 -0400

    Added more to register_spec

--- change_machine.rb
+++ change_machine.rb
@@ -1,21 +1,18 @@
 class Register
 
-  puts "Please enter an amount of change from 0-100 cents"
-  
-  change = gets.chomp.to_i
 
-  quarters = change/25 
-  dimes = (change - (quarters*25))/10
-  nickels = (change - ((quarters*25)+(dimes*10)))/5
-  pennies = (change - ((quarters*25)+(dimes*10)+(nickels*5)))/1
+    puts "Please enter an amount of change from 0-100 cents"
+    change = gets.chomp.to_i
 
+    quarters = change/25 
+    dimes = (change - (quarters*25))/10
+    nickels = (change - ((quarters*25)+(dimes*10)))/5
+    pennies = (change - ((quarters*25)+(dimes*10)+(nickels*5)))/1
 
-
-  print quarters.to_s + " quarters \n" 
-  print dimes.to_s + " dimes \n"
-  print nickels.to_s + " nickels \n"
-  print pennies.to_s + " pennies \n"
+    print quarters.to_s + " quarters \n" 
+    print dimes.to_s + " dimes \n"
+    print nickels.to_s + " nickels \n"
+    print pennies.to_s + " pennies \n"
 
 end
-
 
--- spec/register_spec.rb
+++ spec/register_spec.rb
@@ -1,8 +1,21 @@
-require_relative '../change_machine'
+require 'rspec'
+require_relative'../change_machine'
 
 describe Register do 	
-  it "returns an number" do 
-    it { should be_kind_of(Fixnum) } 
+  before do 
+    @register = Register.new
   end
+
+  it "should prompt" do 
+    STDOUT.should_receive(:puts).with("Please enter an amount of change from 0-100 cents")
+  end
+
+   it "should give correct change for multiples of 25" do
+
+   end
+
+   it "should give correct change for values returning at least one of each coin" do
+
+   end
 end
 
